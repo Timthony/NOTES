3031371116def49d48f672974cbbffc3e6d3fbea
Timthony <tianhangz1023@gmail.com>
2018-10-30 22:47:11 +0800

    Update 349. 两个数组的交集.cpp

--- leetcode/349. 两个数组的交集.cpp
+++ leetcode/349. 两个数组的交集.cpp
@@ -15,3 +15,24 @@ public:
         return chong;
     }
 };
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> nums1Map;
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            nums1Map[nums1[i]]++;
+        }
+        vector<int> resVec;
+        for (int j = 0; j < nums2.size(); j++)
+        {
+            if(nums1Map[nums2[j]] > 0)
+            {
+                resVec.push_back(nums2[j]);
+                nums1Map[nums2[j]] = -1;
+            }
+        }
+        return resVec;
+
+    }
+};
