c2caa94b8507e5b50cb2a4ccdae16bab61e11a78
呆 <tianhangz1023@gmail.com>
2019-05-22 11:22:15 +0800

    实现非递归的方式

--- leetcode/101对称二叉树.cpp
+++ leetcode/101对称二叉树.cpp
@@ -40,3 +40,45 @@ public:
         
     }
 };
+\\/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        //非递归方式实现
+        if(root == nullptr)  return true;
+        
+        TreeNode* p1 = root;
+        TreeNode* p2 = root;
+        
+        queue<TreeNode*> left, right;
+        left.push(p1->left);
+        right.push(p2->right);
+        while(!left.empty() && !right.empty())
+        {
+            p1 = left.front();
+            left.pop();
+            p2 = right.front();
+            right.pop();
+            //如果P1和p2同时为空或者同时不为空才执行
+            if(p1==nullptr&&p2==nullptr)  continue;
+            else if(!p1 || !p2)  return false;
+            else if(p1->val != p2->val)  return false;
+
+            left.push(p1->left);
+            left.push(p1->right);
+            right.push(p2->right);
+            right.push(p2->left);
+        }
+        
+        return true;  
+        
+    }
+};
